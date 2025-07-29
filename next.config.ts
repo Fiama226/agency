7f759ef21eadaa4104e1809974f00e492cfca25d
Fiama226 <92838877+Fiama226@users.noreply.github.com>
2025-07-29 12:26:27 +0000

    updated1

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig: NextConfig = {
-     eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig: NextConfig = {};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
